7a6b27152b8e3909cd2ca585adb907b2c110a870
Harry Wadman <harry.wadman4@gmail.com>
2018-11-29 11:58:39 +0000

    Added picking up, putting down, and using items as well as secret doors

--- Classes.py
+++ Classes.py
@@ -5,19 +5,63 @@ class Adventurer(object):
         self.things = things;
 
     def __str__(self):
-        list = " ".join(self.things)
-        return(f"You are currently in position {self.xpos},{self.ypos} and are carrying {list}")
+        list = []
+        for thing in self.things:
+            list.append(thing.name)
+        return(f"You are currently carrying {list}")
 
 
 class Room(object):
-    def __init__(self, xpos, ypos, things, doors):
+    def __init__(self, xpos, ypos, things, doors, desc):
         self.xpos = xpos;
         self.ypos = ypos;
         self.things = things;
         self.doors = doors;
+        self.desc = desc;
+
+    def __str__(self):
+        out = ""
+        out2 = ""
+        out3 = ""
+        out += f"You are in a {self.desc}. "
+        if self.doors[0] == 1:
+            if out2 == "":
+                out2 += "There is a door to the north"
+            else:
+                out2 += ", north"
+        if self.doors[1] == 1:
+            if out2 == "":
+                out2 += "There is a door to the east"
+            else:
+                out2 += ", east"
+        if self.doors[2] == 1:
+            if out2 == "":
+                out2 += "There is a door to the south"
+            else:
+                out2 += ", south"
+        if self.doors[3] == 1:
+            if out2 == "":
+                out2 += "There is a door to the west"
+            else:
+                out2 += ", west"
+        if len(self.things) > 0:
+            out3 += "There is a "
+            for obj in self.things:
+                out3 += obj.name
+                if len(self.things) != 1:
+                    out3 += ", "
+                else:
+                    out3+= " "
+            out3 += "in the room. "
+        out += out3
+        out += out2
+        out += ".\n"
+        return(out)
 
 class Thing(object):
-    def __init__(self, name, desc, onUse):
+    def __init__(self, name, desc):
         self.name = name;
         self.desc = desc;
-        self.onUse = onUse;
+
+    def __str__(self):
+        return self.desc
--- Main.py
+++ Main.py
@@ -4,20 +4,18 @@ import sys, time
 player = Adventurer(0,0,[])
 rooms = []
 
-rooms.append(Room(0,0,[],[0,1,0,0]))                                            #Room 0,0 (1)
-rooms.append(Room(1,0,[],[1,1,0,1]))                                            #Room 1,0 (2)
-rooms.append(Room(2,0,[],[0,1,0,1]))                                            #Room 2,0 (3)
-rooms.append(Room(3,0,[],[1,0,0,1]))                                            #Room 3,0 (4)
-rooms.append(Room(0,1,[],[1,1,0,0]))                                            #Room 0,1 (5)
-rooms.append(Room(1,1,[],[0,1,1,1]))                                            #Room 1,1 (6)
-rooms.append(Room(2,1,[],[0,1,0,1]))                                            #Room 2,1 (7)
-rooms.append(Room(3,1,[],[0,0,1,1]))                                            #Room 3,1 (8)
-rooms.append(Room(0,2,[],[0,1,1,0]))                                            #Room 0,2 (9)
-rooms.append(Room(1,2,[],[0,1,0,1]))                                            #Room 1,2 (10)
-rooms.append(Room(2,2,[],[0,1,0,1]))                                            #Room 2,2 (11)
-rooms.append(Room(3,2,[],[0,0,0,1]))                                            #Room 3,2 (12)
+rooms.append(Room(0,0,[],[1,1,0,0],"dull concrete room"))                       #Room 0,0 (1)
+rooms.append(Room(1,0,[],[1,0,0,1],""))                                         #Room 1,0 (2)
+rooms.append(Room(0,1,[],[0,1,1,0],""))                                         #Room 0,1 (3)
+rooms.append(Room(1,1,[],[0,0,1,1],""))                                         #Room 1,1 (4)
+rooms.append(Room(0,2,[],[0,0,1,0],""))                                         #Room 0,2 (5)
+rooms.append(Room(1,2,[],[0,0,0,1],""))                                         #Room 1,2 (6)
+
+rooms[1].things.append(Thing("candle", "A lit candle, it has not melted at all.\n"))
+rooms[3].things.append(Thing("brick", "An ordinary brick. It looks like it weighs quite a bit.\n"))
 
 def SlowPrint(words):
+    print("\n")
     for letter in words:
         sys.stdout.write(letter)
         sys.stdout.flush()
@@ -37,15 +35,45 @@ def Command(word):
         if wordList[1] == "west" or wordList[1] == "w":
             Move(3)
 
-    if wordList[0] == "look":
-        print("look here")
+    if wordList[0] == "look" and len(wordList) <= 1:
+        roomToLook = Room(0,0,[],[],"")
+        roomToLook = GetRoom(player.xpos, player.ypos)
+        SlowPrint(roomToLook.__str__())
+    if wordList[0] == "look" and len(wordList) > 1:
+        roomTemp1 = GetRoom(player.xpos, player.ypos)
+        for obj in roomTemp1.things:
+            if obj.name == wordList[1]:
+                SlowPrint(obj.desc)
+        for obj in player.things:
+            if obj.name == wordList[1]:
+                SlowPrint(obj.desc)
+
+    if wordList[0] == "take":
+        TakeItem(wordList[1])
+        SlowPrint(player.__str__())
+
+    if wordList[0] == "drop":
+        DropItem(wordList[1])
+
+    if wordList[0] == "use":
+        playerRoom = GetRoom(player.xpos, player.ypos)
+
+        if wordList[1] == "candle":
+            if playerRoom.xpos == 0 and playerRoom.ypos == 1:
+                SlowPrint("You use the candle to burn the rope, a secret door appears to the north.")
+                playerRoom.doors[0] = 1
+            else:
+                SlowPrint("Nothing happens.")
+        if wordList[1] == "brick":
+            if playerRoom.xpos == 0 and playerRoom.ypos == 2:
+                SlowPrint("You put the brick on the scale, a scret door appears to the east.")
+                playerRoom.doors[1] = 1
+            else:
+                SlowPrint("Nothing happens.")
 
 def Move(direction):
     playerRoom = rooms[0]
-    for room in rooms:
-        if player.xpos == room.xpos and player.ypos == room.ypos:
-            playerRoom = room
-
+    playerRoom = GetRoom(player.xpos, player.ypos)
     if playerRoom.doors[direction] == 1:
         if direction == 0:
             player.ypos += 1
@@ -57,15 +85,46 @@ def Move(direction):
             player.xpos -= 1
         SlowPrint("You move into a diffrent room \n")
         return True
-
     SlowPrint("You cannot move in that direction! \n")
     return False
 
+def GetRoom(roomx, roomy):
+    for room in rooms:
+        if roomx == room.xpos and roomy == room.ypos:
+            return room
+
+def CheckRoom(thingToCheck, room):
+    if room.things > 0:
+        for obj in room.things:
+            if obj == thingToCheck:
+                return True
+        return False
+    else:
+        return False
+
+def CheckPlayer(thingToCheck):
+    if player.things > 0:
+        for obj in player.things:
+            if obj == thingToCheck:
+                return True
+        return False
+    else:
+        return False
+
+def TakeItem(itemName):
+    roomTemp2 = GetRoom(player.xpos, player.ypos)
+    for obj in roomTemp2.things:
+        if obj.name == itemName:
+            player.things.append(obj)
+            roomTemp2.things.remove(obj)
+
+def DropItem(itemName):
+    roomTemp3 = GetRoom(player.xpos, player.ypos)
+    for obj in player.things:
+        if obj.name == itemName:
+            roomTemp3.things.append(obj)
+            player.things.remove(obj)
+
 while True:
     Command(input(""))
-    print(player)
-    sudobreak = input("BREAK?:")
-    if sudobreak == "y":
-        break
-
-#SlowPrint("My name is jeff and I am a bard traveling the world to share my tales! Who are you? \n")
+    print("\n")
